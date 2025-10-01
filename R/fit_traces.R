970de416fe1f36927f2c8aab5b0e2e6dc7648c28
Jonathan Kennel <jkennel@uoguelph.ca>
2025-10-01 17:48:03 -0400

    use rlm

--- R/fit_traces.R
+++ R/fit_traces.R
@@ -41,11 +41,11 @@ prep_fit <- function(x, start_time, end_time, include_intercept = FALSE) {
   input <- log(unique(x[['elapsed_time']]))
 
   if (include_intercept) {
-    fit <- lm(output ~ input)
+    fit <- MASS::rlm(output ~ input)
   } else {
     output <- output - output[1, ] # remove the first value
     input <- input - input[1] # remove the first value
-    fit <- lm(output ~ input - 1) # removed intercept
+    fit <- MASS::rlm(output ~ input - 1) # removed intercept
   }
 
   out <- data.table(distance = unique(x[['distance']]))

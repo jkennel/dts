d0fa9ea8030eeaac1170bd2641d9bc63908348c5
Jonathan Kennel <jkennel@uoguelph.ca>
2025-10-06 15:50:43 -0400

    fix problem with duplicate start times

--- R/to_matrix.R
+++ R/to_matrix.R
@@ -1,7 +1,7 @@
 #' to_matrix
 #'
 #' @param x data read from a dts
-#' @param ... 
+#' @param ...
 #'
 #' @return
 #' @rdname to_matrix
@@ -11,62 +11,58 @@
 to_matrix <- function(x, ...) UseMethod("to_matrix")
 
 
-
 #' @rdname to_matrix
 #' @export
 to_matrix.dts <- function(x, ...) {
-  
   wh <- x$event_info$wh
   distance <- x$event_info$distance[wh]
-  
-  tr <- (map(x$dts, .f = function(x, ...) {
-    temperature = x$trace_data[wh]
-  }, wh = wh))
-  
-  start_times <- get_times(dts)
-  
+
+  tr <- (map(
+    x$dts,
+    .f = function(x, ...) {
+      temperature = x$trace_data[wh]
+    },
+    wh = wh
+  ))
+
+  start_times <- sort(unique(get_times(dts)))
+
   m <- matrix(unlist(tr), nrow = length(wh))
-  
+
   rownames(m) <- distance
   colnames(m) <- start_times
-  
+
   m
-  
 }
 
 
 #' @rdname to_matrix
 #' @export
 to_matrix.data.table <- function(x, col_name = 'temperature') {
-  
   setkey(x, start, distance)
-  
+
   distance <- unique(x[['distance']])
   start <- unique(x[['start']])
   nr <- length(distance)
-  
-  m  <- matrix(x[[col_name]], nrow = nr)
-  
+
+  m <- matrix(x[[col_name]], nrow = nr)
+
   rownames(m) <- distance
   colnames(m) <- start
-  
+
   m
 }
 
 #' @rdname to_matrix
 #' @export
 to_matrix.dts_long <- function(x, col_name = 'temperature') {
-  
   to_matrix(get_data_table(x), col_name)
-  
 }
-
 
 #' #' @rdname to_matrix
 #' #' @export
 #' to_matrix.dts_time_subset <- function(x, col_name = 'temperature') {
-#'   
+#'
 #'   to_matrix(get_data_table(x), col_name)
-#'   
+#'
 #' }
-
